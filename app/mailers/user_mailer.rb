5e71b5253136ea19d747070d1b91b3535f0e35af
cameronjacoby <cameron.jacoby@gmail.com>
2016-02-01 13:52:36 -0800

    remove () from user_mailer#welcome

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome(user)
   	@user = user
-    mail( to: @user.email, subject: "Thanks for signing up!" )
+    mail to: @user.email, subject: "Thanks for signing up!"
   end
 
 end
